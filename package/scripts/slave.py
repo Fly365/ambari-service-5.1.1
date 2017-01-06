a10de61ea917fd926c6d84bcfe1a97331cc60448
Phakin Cheangkrachange <phakin.che@gmail.com>
2017-01-06 17:57:49 +0700

    Remove recursive

--- package/scripts/slave.py
+++ package/scripts/slave.py
@@ -15,16 +15,15 @@ def slave():
     params.path_data = params.path_data.replace('"','')
     data_path = params.path_data.replace(' ','').split(',')
     data_path[:]=[x.replace('"','') for x in data_path]
-    
+
     directories = [params.log_dir, params.pid_dir, params.conf_dir]
     directories = directories+data_path;
-    
+
     Directory(directories,
               owner=params.elastic_user,
-              group=params.elastic_user,
-              recursive=True
+              group=params.elastic_user
           )
-    
+
     File(format("{conf_dir}/elastic-env.sh"),
           owner=params.elastic_user,
           content=InlineTemplate(params.elastic_env_sh_template)
@@ -39,7 +38,7 @@ def slave():
        owner=params.elastic_user,
        group=params.elastic_user
     )
-    
+
     File(format("/etc/sysconfig/elasticsearch"),
        content=Template(
                         "elasticsearch.sysconfig.j2",
